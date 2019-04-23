e98aa196398332692d19bc9e8e960b225c26994f
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-04-23 12:37:45 +0900

    Create NetMgr.cs

---
+++ src/NewWirelessAutomatic/NetMgr.cs
@@ -0,0 +1,9 @@
+using NetworkManager;
+
+namespace NewWirelessAutomatic
+{
+    public static class WirelessMgr
+    {
+        public static ChannelNetworkMgrBase NetSystem { get; internal set; }
+    }
+}
