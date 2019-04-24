1a4dea6e249e0d373a08642c98406650c5821161
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-04-24 22:16:45 +0900

    특수 채널 지원을 위한 코드 추가됨 (실험적)

--- src/NewWirelessAutomatic/SignalEmitter.cs
+++ src/NewWirelessAutomatic/SignalEmitter.cs
@@ -8,11 +8,10 @@ namespace NewWirelessAutomatic
     using static WirelessMgr;
     using static NWA_CONST;
     [SerializationConfig(MemberSerialization.OptIn)]
-    public class SignalEmitter : KMonoBehaviour, IIntSliderControl, IChannelNetworkComponent
+    public class SignalEmitter : KMonoBehaviour, IIntSliderControl, IChannelNetworkComponentEX
     {
-        protected static IntraObjectHandler<SignalEmitter> OnOperationalChangedDelegate =
-            new IntraObjectHandler<SignalEmitter>(OnOperationalChangedStatic);
-
+        protected static IntraObjectHandler<SignalEmitter> OnOperationalChangedDelegate = new IntraObjectHandler<SignalEmitter>(OnOperationalChangedStatic);
+        
         [MyCmpAdd]
         protected Operational operational;
         [Serialize]
@@ -48,6 +47,8 @@ namespace NewWirelessAutomatic
         public bool GetSignal { get; protected set; }
 
         public ConnectChangeEventHandler GetEventHandler => null;
+
+        public bool GetOperationalState => operational.IsOperational;
         #endregion
 
         #region 본 함수
@@ -59,13 +60,28 @@ namespace NewWirelessAutomatic
             NetSystem.SignalEmit(emitCh, on);
         }
 
-        protected virtual void ChangeChannel(int ch) => emitCh = ch;
+        protected virtual void ChangeChannel(int ch)
+        {
+            NetSystem.OnEmitterDisconenct(this);
+            emitCh = ch;
+            NetSystem.OnEmitterConnect(this);
+        } 
 
         protected override void OnPrefabInit() => Subscribe(OperationalChanged, OnOperationalChangedDelegate);
 
-        protected override void OnSpawn() => OnOperationalChangeSafe(operational.IsOperational);
+        protected override void OnSpawn() => ChangeChannel(emitCh);
 
         protected override void OnCleanUp() => Unsubscribe(OperationalChanged, OnOperationalChangedDelegate);
+        
+        // True를 반환하는 경우 Subscribe 함수 본문을 무시합니다.
+        public bool PreSubscribe(INetworkMgr<IChannelNetworkComponent> sender, object args) => false;
+        
+        public void PostSubscribe(INetworkMgr<IChannelNetworkComponent> sender, object args) { }
+        
+        // True를 반환하는 경우 Unsubscribe 함수  무시합니다.
+        public bool PreUnsubscribe(INetworkMgr<IChannelNetworkComponent> sender, object args) => false;
+
+        public void PostUnsubscribe(INetworkMgr<IChannelNetworkComponent> sender, object args) { }
         #endregion
     }
 }
