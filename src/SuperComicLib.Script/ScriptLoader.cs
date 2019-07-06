5e54cbd4e2dfa7f755a11c08c2eef84b618ef5b0
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-07-07 03:34:56 +0900

    Add files via upload

---
+++ src/SuperComicLib.Script/ScriptLoader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace SuperComicLib.Script
+{
+    public static class ScriptLoader
+    {
+        internal static Dictionary<string, IDynamicLinker> linkers;
+        private static readonly Type ifces = typeof(IDynamicLinker);
+        private static readonly Type atsearch = typeof(PublicSearchableAttribute);
+        private static readonly Type atci = typeof(CreateInstanceAttribute);
+        private const string SEARCH_NAME = "CreateInstance";
+        private const BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+        public static void Load(string diretory_path)
+        {
+            if (linkers != null)
+                return;
+            linkers = new Dictionary<string, IDynamicLinker>();
+            Internal_Load_Dlls(Directory.GetFiles(diretory_path, "*.dll", SearchOption.AllDirectories));
+        }
+
+        private static void Internal_Load_Dlls(string[] files)
+        {
+            Type[] tps;
+            MethodInfo minfo;
+            foreach (string x in files)
+            {
+                if (x.Contains("SuperComicLib"))
+                    continue;
+                try
+                {
+                    tps = Assembly.LoadFile(x).GetTypes();
+                    foreach (Type i in tps)
+                        if (i.GetCustomAttributes(atsearch, false).Length > 0)
+                        {
+                            minfo = i.GetMethod(SEARCH_NAME, flags) ?? FindFirst(i.GetMethods(flags), c => c.GetCustomAttributes(atci, false).Length > 0);
+                            if (minfo != null && minfo.ReturnType == ifces)
+                                linkers.Add(Path.GetFileNameWithoutExtension(x), (IDynamicLinker)minfo.Invoke(null, null));
+                        }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        private static T FindFirst<T>(IEnumerable<T> vs, Func<T, bool> func)
+        {
+            foreach (T a in vs)
+                if (func.Invoke(a))
+                    return a;
+            return default;
+        }
+
+        public static bool GetValue(string name, out IDynamicLinker value) => linkers.TryGetValue(name, out value);
+
+        public static void Add(string name, IDynamicLinker linker) => linkers.Add(name, linker);
+    }
+}
---
+++ src/SuperComicLib.Script/Types.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SuperComicLib.Script
+{
+    public delegate void DynamicActionDelegate(object component, object value);
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+    public sealed class PublicSearchableAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public sealed class CreateInstanceAttribute : Attribute { }
+
+    public enum TriggerType : byte
+    {
+        Default = 1,
+        Control = 2,
+        All = byte.MaxValue
+    }
+
+    public interface IDynamicLinker
+    {
+        Type GetComponentType { get; }
+
+        TriggerType AssignTo { get; }
+
+        void EventAction(object component, object value);
+    }
+
+    public interface ITriggerControl
+    {
+        void SendSignal(bool ison);
+
+        bool InputState { get; }
+    }
+
+    public sealed class GetComponentTypes
+    {
+        public static class None { public const string ID = nameof(None); }
+        public static class GetAllEntities { public const string ID = nameof(GetAllEntities); }
+        public static class GetDuplicants { public const string ID = nameof(GetDuplicants); }
+        public static class GetAnimals { public const string ID = nameof(GetAnimals); }
+        public static class GetGameObject { public const string ID = nameof(GetGameObject); }
+    }
+
+    public struct DynamicLinker : IDynamicLinker
+    {
+        public DynamicLinker(DynamicActionDelegate act) : this() => FuncAction = act;
+
+        public DynamicLinker(DynamicActionDelegate act, Type rettype)
+        {
+            FuncAction = act;
+            GetComponentType = rettype;
+            AssignTo = TriggerType.All;
+        }
+
+        public DynamicLinker(DynamicActionDelegate act, Type rettype, TriggerType type)
+        {
+            FuncAction = act;
+            GetComponentType = rettype;
+            AssignTo = type;
+        }
+
+        public static DynamicLinker Make<T>(DynamicActionDelegate act) => new DynamicLinker(act, typeof(T));
+
+        public static DynamicLinker Make<T>(DynamicActionDelegate act, TriggerType tp) => new DynamicLinker(act, typeof(T), tp);
+
+        void IDynamicLinker.EventAction(object component, object value) => FuncAction.Invoke(component, value);
+
+        public Type GetComponentType { get; private set; }
+        public DynamicActionDelegate FuncAction { get; private set; }
+        public TriggerType AssignTo { get; private set; }
+    }
+}
