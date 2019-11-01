280cfdcf9212b67b4285cce06458e554a5b58031
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-11-02 00:08:46 +0900

    Update EXS.cs

--- src/3SuperComicLib.HUSystem/Util/EXS.cs
+++ src/3SuperComicLib.HUSystem/Util/EXS.cs
@@ -1,10 +1,8 @@
-﻿namespace SuperComicLib.HUSystem
+namespace SuperComicLib.HUSystem
 {
     public static class ObjdctEXPlus
     {
-        // public static bool HasFlag(this HUEndpointType target, HUEndpointType val) => (target & val) != 0;
-        public static int GetRotatedOffsetCell(this Building bd, CellOffset cell) =>
-            Grid.OffsetCell(Grid.PosToCell(bd.transform.GetPosition()), cell);
+        public static int GetRotatedOffsetCell(this Building bd, CellOffset cell) => Grid.OffsetCell(Grid.PosToCell(bd.transform.GetPosition()), cell);
 
         public static bool HasFlag(this UtilityConnections target, UtilityConnections val) => (target & val) != 0;
     }
