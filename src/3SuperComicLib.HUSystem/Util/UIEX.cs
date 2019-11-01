d3c932942070a74206b917664c474782a0665da2
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-11-02 00:08:16 +0900

    Add files via upload

---
+++ src/3SuperComicLib.HUSystem/Util/UIEX.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperComicLib.HUSystem
+{
+    using static STRINGS.UI;
+    public static class UIEX
+    {
+        public static readonly LocString HUPERSEC_PRODUCE = $"연결된 {PRE_KEYWORD}히트파이프{PST_KEYWORD}에 {PRE_RATE_POSITIVE}{{0}}/s{PST_RATE} 속도로 <b>HU</b>를 공급합니다.";
+        public static readonly LocString HUPERSEC_CONSUMED = $"{PRE_KEYWORD}{{0}} {{1}}/s{PST_KEYWORD} 속도로 <b>HU</b>를 소비";
+    }
+}
