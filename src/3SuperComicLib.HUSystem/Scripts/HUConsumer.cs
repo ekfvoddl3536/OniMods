3e01b633fd543d839056ff25ac5f0abfa031cf27
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-11-02 10:29:34 +0900

    Update HUConsumer.cs

--- src/3SuperComicLib.HUSystem/Scripts/HUConsumer.cs
+++ src/3SuperComicLib.HUSystem/Scripts/HUConsumer.cs
@@ -1,27 +1,19 @@
-﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace SuperComicLib.HUSystem
 {
-    using System;
-    using UnityEngine;
-    using UnityEngine.UI;
     using static Constants;
     public class HUConsumer : KMonoBehaviour, ISaveLoadable, IHUConsumer, IEffectDescriptor
     {
-        public static readonly Operational.Flag heatedFlag = new Operational.Flag("Heated", Operational.Flag.Type.Requirement);
-
         public CellOffset offset;
         [MyCmpReq]
         protected Building building;
         [MyCmpGet]
         protected Operational operational;
         public int huMinimum, huMaximum;
-
-        protected virtual bool IsHeated
-        {
-            get => operational.GetFlag(heatedFlag);
-            set => operational.SetFlag(heatedFlag, value);
-        }
 
         public virtual bool IsConnected
         {
@@ -33,8 +25,6 @@ namespace SuperComicLib.HUSystem
         public virtual int HUMin => huMinimum;
         public virtual int HUCell { get; protected set; }
 
-        public virtual ushort ID => manager.GetID(HUCell);
-
         public virtual string ObjectName => gameObject.name;
 
         public virtual string ToolTipText => "HU Consumed";
@@ -44,28 +34,20 @@ namespace SuperComicLib.HUSystem
             HUCell = building.GetRotatedOffsetCell(offset);
             manager.Connect(this);
 
-            IsHeated = false;
             IsConnected = false;
         }
 
         protected override void OnCleanUp()
         {
-            IsConnected = IsHeated = false;
+            IsConnected = false;
             manager.Disconnect(this);
         }
 
         public virtual List<Descriptor> GetDescriptors(BuildingDef def) => null;
-        public virtual int ConsumedHU(int HUavailable, float dt)
-        {
-            IsHeated = true;
-            return OnConsumedHU(Math.Min(HUMax, HUavailable), dt);
-        }
+        
+        public virtual int ConsumedHU(int HUavailable, float dt) => Math.Min(HUMax, HUavailable);
 
-        protected virtual int OnConsumedHU(int hu, float dt) => hu;
-
-        public virtual void HUSim200ms(float dt) { }
-
-        public virtual void SetConnectionState(bool connected) => IsConnected = connected;
+        public virtual void HUSim200ms(float dt) => IsConnected = manager.GetID(HUCell) != ushort.MaxValue;
 
         public virtual bool IfAddLabel(ICollection<SaveLoadRoot> roots, Vector2I min, Vector2I max)
         {
