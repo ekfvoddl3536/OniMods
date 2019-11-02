b8053db75e0a34f7600288a51733136deb9a8eb0
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-11-02 10:29:52 +0900

    Update Types.cs

--- src/3SuperComicLib.HUSystem/Types/Types.cs
+++ src/3SuperComicLib.HUSystem/Types/Types.cs
@@ -31,7 +31,6 @@ namespace SuperComicLib.HUSystem
         int HUMax { get; }
         int HUMin { get; }
 
-        void SetConnectionState(bool connected);
         int ConsumedHU(int HUavailable, float dt);
     }
 }
