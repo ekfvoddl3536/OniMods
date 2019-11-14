63ab934808af5a729685d0fccd8ccee5a4a0b9e7
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-11-14 14:19:37 +0900

    Add files via upload

---
+++ src/4SuperComicLib.Threading/Threading/AwaitibleAsyncThread.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SuperComicLib.Threading
+{
+    public delegate IEnumerator<Action> AwaitibleAsyncMethod();
+
+    public sealed class AwaitibleAsyncThread
+    {
+        private bool skipframe;
+        private bool working;
+        private ManualResetEvent revent = new ManualResetEvent(false);
+        private Action action;
+
+        public IEnumerator StartThread(AwaitibleAsyncMethod asyncMethod)
+        {
+            skipframe = true;
+            working = true;
+            new Thread(() => SubThreadWork(asyncMethod)).Start();
+            while (working)
+                if (skipframe) yield return null;
+                else
+                {
+                    skipframe = true;
+                    action.Invoke();
+                    revent.Set();
+                }
+            yield break;
+        }
+
+        private void SubThreadWork(AwaitibleAsyncMethod method)
+        {
+            IEnumerator<Action> enumerator = method.Invoke();
+            while (enumerator.MoveNext())
+                if (enumerator.Current != null)
+                {
+                    action = enumerator.Current;
+                    skipframe = false;
+                    revent.WaitOne();
+                }
+            working = false;
+        }
+    }
+}
---
+++ src/4SuperComicLib.Threading/Threading/ScheduledAsyncTask.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SuperComicLib.Threading
+{
+    public sealed class ScheduledAsyncTask
+    {
+        private bool workThreadBlocking;
+        private bool working;
+        private ManualResetEvent revent = new ManualResetEvent(false);
+        private Action action;
+        private Queue<AwaitibleAsyncMethod> targets = new Queue<AwaitibleAsyncMethod>();
+
+        public IEnumerator Start()
+        {
+            workThreadBlocking = true;
+            working = true;
+            new Thread(SubThreadWork);
+            while (working)
+                if (workThreadBlocking) yield return null;
+                else
+                {
+                    workThreadBlocking = true;
+                    action.Invoke();
+                    revent.Set();
+                }
+            yield break;
+        }
+
+        public void Scheduled(AwaitibleAsyncMethod method)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+            targets.Enqueue(method);
+        }
+
+        private void SubThreadWork()
+        {
+            while (targets.Count > 0)
+            {
+                IEnumerator<Action> temp = targets.Dequeue().Invoke();
+                while (temp.MoveNext())
+                {
+                    workThreadBlocking = false;
+                    if (temp.Current != null)
+                    {
+                        action = temp.Current;
+                        revent.WaitOne();
+                    }
+                }
+            }
+            working = false;
+        }
+    }
+}
